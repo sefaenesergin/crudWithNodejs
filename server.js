8faaabfe434095320e3b725be44b5f67f0160d65
sefaenesergin <sefaenes07@icloud.com>
2022-10-23 00:00:00 +0300

    third commit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const dotenv = require("dotenv");
 const app = express();
 const morgan = require("morgan");
 const bodyparser = require("body-parser");
-const path = require('path')
+const path = require("path");
 dotenv.config({ path: "config.env" });
 const PORT = process.env.PORT || 8080;
 //prettier oto save :option+shift+f
@@ -12,20 +12,18 @@ const PORT = process.env.PORT || 8080;
 app.use(morgan("tiny"));
 
 //parse request to bodyparser
-app.use(bodyparser.urlencoded({extended:true}))
+app.use(bodyparser.urlencoded({ extended: true }));
 
 //set view engine
-app.set("view engine","ejs")
-
+app.set("view engine", "ejs");
 
 //load assets
-app.use('/css',express.static(path.resolve(__dirname,"assets/css")))
-app.use('/css',express.static(path.resolve(__dirname,"assets/img")))
-app.use('/css',express.static(path.resolve(__dirname,"assets/js")))
-
+app.use("/css", express.static(path.resolve(__dirname, "assets/css")));
+app.use("/img", express.static(path.resolve(__dirname, "assets/img")));
+app.use("/js", express.static(path.resolve(__dirname, "assets/js")));
 
 app.get("/", (req, res) => {
-  res.send("crud app is ready");
+  res.render("index.ejs");
 });
 
 app.listen(3000, () => {
