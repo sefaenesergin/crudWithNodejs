c6c5bea77f9f3ecc542a9af173a1f1130dad9e39
sefaenesergin <sefaenes07@icloud.com>
2022-10-23 23:48:45 +0300

    create operation is done

---
+++ server/controller/controller.js
@@ -0,0 +1,38 @@
+var Userdb = require("../model/model");
+
+//create and save new user
+exports.create = (req, res) => {
+  //validate request
+  if (!req.body) {
+    res.status(400).send({ message: "Content can not be empty!" });
+    return;
+  }
+  //new user
+  const user = new Userdb({
+    name: req.body.name,
+    email: req.body.email,
+    gender: req.body.gender,
+    status: req.body.status,
+  });
+
+  //save user in the database
+  user
+    .save(user)
+    .then(data=>{
+        res.send(data)
+    })
+    .catch(err=>{
+        res.status(500).send({
+            message: err.message || "some error occured while creating a create operation"
+        })
+    })
+};
+
+//return all user
+exports.find = (req, res) => {};
+
+//update a new idetified user by user id
+exports.update = (req, res) => {};
+
+//delete a user with specified user id in the request
+exports.delete = (req, res) => {};
